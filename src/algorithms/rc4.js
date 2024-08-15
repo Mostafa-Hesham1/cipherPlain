a05adeb0d68ecc7fb0af70341b43b07941968db2
Mohamedbasem1 <medobasem333@gmail.com>
2024-08-15 22:39:43 +0300

    RC4 Algorithem

---
+++ src/algorithms/rc4.js
@@ -0,0 +1,54 @@
+function rc4KeySchedule(key) {
+    let S = [];
+    let T = [];
+
+    for (let i = 0; i < 256; i++) {
+        S[i] = i;
+        T[i] = key.charCodeAt(i % key.length);
+    }
+
+    let j = 0;
+    for (let i = 0; i < 256; i++) {
+        j = (j + S[i] + T[i]) % 256;
+        [S[i], S[j]] = [S[j], S[i]];
+    }
+
+    return S;
+}
+
+function rc4GenerateKeystream(S, textLength) {
+    let i = 0, j = 0;
+    let keystream = [];
+
+    for (let n = 0; n < textLength; n++) {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+
+        [S[i], S[j]] = [S[j], S[i]];
+
+        let t = S[(S[i] + S[j]) % 256];
+        keystream.push(t);
+    }
+
+    return keystream;
+}
+
+export function rc4Encrypt(plaintext, key) {
+    const S = rc4KeySchedule(key);
+    const keystream = rc4GenerateKeystream(S, plaintext.length);
+    let ciphertext = '';
+
+    for (let i = 0; i < plaintext.length; i++) {
+        const ptByte = plaintext.charCodeAt(i);
+        const keyByte = keystream[i];
+        const ctByte = ptByte ^ keyByte;
+        ciphertext += String.fromCharCode(ctByte);
+    }
+
+    return { ciphertext, keystream };
+}
+
+export function rc4Decrypt(ciphertext, key) {
+    // RC4 decryption is symmetric, same function as encryption
+    return rc4Encrypt(ciphertext, key);
+}
