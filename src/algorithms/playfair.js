342c8660ec5792328280f6ed0450847adfaa0774
Mostafa-Hesham1 <141531524+Mostafa-Hesham1@users.noreply.github.com>
2024-08-18 05:29:26 +0300

    Update playfair.js

--- src/algorithms/playfair.js
+++ src/algorithms/playfair.js
@@ -1,60 +1,92 @@
 function createMatrix(key) {
-    const alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; // J treated as I
+    const alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; 
     let matrixString = "";
-    let used = new Set();
+
+     let used = new Set();
 
     key = key.toUpperCase().replace(/[^A-Z]/g, '').replace(/J/g, 'I');
 
-    for (let char of key) {
-        if (!used.has(char) && alphabet.includes(char)) {
-            matrixString += char;
-            used.add(char);
+    for (let I of key) {
+
+        if (!used.has(I) && alphabet.includes(I)) {
+
+            matrixString += I;
+
+             used.add(I);
+             //console.log(used)
         }
     }
 
-    for (let char of alphabet) {
-        if (!used.has(char)) {
-            matrixString += char;
-            used.add(char);
+    for (let I of alphabet) {
+
+
+         if (!used.has(I)) {
+            matrixString += I;
+
+
+            used.add(I);
         }
     }
 
     const matrix = [];
-    for (let i = 0; i < 5; i++) {
+     for (let i = 0; i < 5; i++) {
         matrix.push(matrixString.slice(i * 5, i * 5 + 5).split(''));
     }
-
-    return matrix;
+     console.log(matrix);
+     return matrix;
 }
 
-function prepareText(text, substitute = 'X') {
+function prepareText(text) {
+    
     text = text.toUpperCase().replace(/J/g, 'I').replace(/[^A-Z]/g, '');
     let pairs = [];
+    const alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; 
     
     for (let i = 0; i < text.length; i += 2) {
         let pair = text[i];
         
-        // If the next character is the same, insert the substitute
-        if (i + 1 < text.length) {
+        if (i + 1 < text.length) { 
             if (text[i] === text[i + 1]) {
-                pair += substitute;
-                i--;  // Stay at the same index to process the next pair correctly
-            } else {
+                if (text[i] === 'X') {
+
+                    pair += text.includes('Z') ? chooseAlternateLetter(text) : 'Z';
+                } else {
+                    pair += 'X';
+                }
+                i--; 
+            } 
+            else {
                 pair += text[i + 1];
             }
-        } else {
-            pair += substitute;  // Pad with 'X' if we reach the last character and it's alone
+        } 
+        else {
+            pair += text[i] === 'X' ? (text.includes('Z') ? chooseAlternateLetter(text) : 'Z') : 'X'; 
         }
         
         pairs.push(pair);
     }
-
+//console.log(pairs)
     return pairs;
 }
 
+function chooseAlternateLetter(text) {
+
+    const alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; 
+    
+    for (let char of alphabet) {
+         if (!text.includes(char)) {
+            return char;
+        }
+    }
+     return 'X';
+}
+
+
 function findPosition(letter, matrix) {
     for (let row = 0; row < matrix.length; row++) {
-        for (let col = 0; col < matrix[row].length; col++) {
+
+         for (let col = 0; col < matrix[row].length; col++)
+             {
             if (matrix[row][col] === letter) {
                 return { row, col };
             }
@@ -75,19 +107,27 @@ export function playfairEncrypt(text, key) {
 
     pairs.forEach(pair => {
         const pos1 = findPosition(pair[0], matrix);
+
         const pos2 = findPosition(pair[1], matrix);
         let result = '';
 
         if (pos1.row === pos2.row) {
+
             result = matrix[pos1.row][(pos1.col + 1) % 5] + matrix[pos2.row][(pos2.col + 1) % 5];
-            steps.push(`Encrypt pair ${pair}: Same row, move right -> ${result}`);
-        } else if (pos1.col === pos2.col) {
+
+             steps.push(`Encrypt pair ${pair}: Same row, move right -> ${result}`);
+        }
+         else if (pos1.col === pos2.col) {
             result = matrix[(pos1.row + 1) % 5][pos1.col] + matrix[(pos2.row + 1) % 5][pos2.col];
             steps.push(`Encrypt pair ${pair}: Same column, move below -> ${result}`);
-        } else {
+        }
+         else {
             result = matrix[pos1.row][pos2.col] + matrix[pos2.row][pos1.col];
             steps.push(`Encrypt pair ${pair}: Different row and column, rectangle swap -> ${result}`);
         }
+
+        //console.log(result)
+
         encryptedText += result;
     });
 
@@ -107,19 +147,24 @@ export function playfairDecrypt(text, key) {
 
     pairs.forEach(pair => {
         const pos1 = findPosition(pair[0], matrix);
-        const pos2 = findPosition(pair[1], matrix);
-        let result = '';
+
+         const pos2 = findPosition(pair[1], matrix);
+         let result = '';
 
         if (pos1.row === pos2.row) {
             result = matrix[pos1.row][(pos1.col + 4) % 5] + matrix[pos2.row][(pos2.col + 4) % 5];
             steps.push(`Decrypt pair ${pair}: Same row, move left -> ${result}`);
-        } else if (pos1.col === pos2.col) {
+        }
+         else if (pos1.col === pos2.col) {
             result = matrix[(pos1.row + 4) % 5][pos1.col] + matrix[(pos2.row + 4) % 5][pos2.col];
             steps.push(`Decrypt pair ${pair}: Same column, move above -> ${result}`);
-        } else {
+        }
+         else {
             result = matrix[pos1.row][pos2.col] + matrix[pos2.row][pos1.col];
+
             steps.push(`Decrypt pair ${pair}: Different row and column, rectangle swap -> ${result}`);
         }
+        //console.log(result)
         decryptedText += result;
     });
 
